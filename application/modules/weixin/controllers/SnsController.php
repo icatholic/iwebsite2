477cb992f7af53ba94682d3b0b483b2407980a0b
yangming <shanghaiyangming@gmail.com>
2014-02-27 15:24:13 +0800

    增加微信授权控制器

--- application/modules/weixin/controllers/SnsController.php
+++ application/modules/weixin/controllers/SnsController.php
@@ -1,1 +1,86 @@
 <?php
+
+class Weixin_SnsController extends Zend_Controller_Action
+{
+
+    private $_weixin;
+
+    private $_user;
+
+    private $_app;
+
+    private $_config;
+
+    public function init()
+    {
+        $this->getHelper('viewRenderer')->setNoRender(true);
+        $this->_config = Zend_Registry::get('config');
+        $this->_user = new Weixin_Model_User();
+        $this->_appConfig = $this->_app->getToken();
+        $this->_weixin = new Weixin\Client();
+        if (! empty($this->_appConfig['access_token'])) {
+            $this->_weixin->setAccessToken($this->_appConfig['access_token']);
+        }
+    }
+
+    /**
+     * http://www.example.com/weixin/sns/index?redirect=回调地址&scope=[snsapi_userinfo(default)|snsapi_base]
+     * 引导用户去往登录授权
+     */
+    public function indexAction()
+    {
+        $redirect = urlencode($this->getRequest()->getParam('redirect', '')); // 附加参数存储跳转地址
+        $scope = $this->getRequest()->getParam('scope', 'snsapi_userinfo');
+        
+        $appid = $this->_appConfig['appid'];
+        $secret = $this->_appConfig['secret'];
+        
+        $moduleName = $this->getRequest()->getModuleName();
+        $controllerName = $this->getRequest()->getControllerName();
+        $actionName = $this->getRequest()->getActionName();
+        
+        $redirectUri = $_SERVER["HTTP_HOST"];
+        $redirectUri .= '/' . $moduleName;
+        $redirectUri .= '/' . $controllerName;
+        $redirectUri .= '/callback';
+        $redirectUri .= '?redirect=' . $redirect;
+        
+        $sns = new \Weixin\Token\Sns($appid, $secret);
+        $sns->setRedirectUri($redirectUri);
+        $sns->setScope($scope);
+        $sns->getAuthorizeUrl();
+    }
+
+    /**
+     * 处理微信的回调数据
+     *
+     * @return boolean
+     */
+    public function callbackAction()
+    {
+        $appid = $this->_appConfig['appid'];
+        $secret = $this->_appConfig['secret'];
+        
+        $sns = new \Weixin\Token\Sns($appid, $secret);
+        $arrAccessToken = $sns->getAccessToken();
+        
+        if (! isset($arrAccessToken['errcode'])) {
+            $_SESSION['iWeixin']['accessToken'] = $arrAccessToken;
+            $redirect = isset($_GET['redirect']) ? urldecode($_GET['redirect']) : '';
+            if (empty($redirect)) {
+                throw new Exception("回调地址未定义");
+            }
+            // 授权成功后，记录该微信用户的基本信息
+            if ($arrAccessToken['scope'] === 'snsapi_userinfo') {
+                $this->_weixin->setSnsAccessToken($arrAccessToken['access_token']);
+                $this->_weixin->getSnsManager()->getSnsUserInfo($arrAccessToken['openid']);
+                $this->_user->update(array());
+            }
+            header("location:{$redirect}");
+            exit();
+        } else {
+            throw new Exception("获取token失败,原因:" . json_encode($arrAccessToken, JSON_UNESCAPED_UNICODE));
+        }
+    }
+}
+
